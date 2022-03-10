ac6f2d8fcbc68d1b04745975b874374de5093fd3
NOobSsam <68308311+NOobSsam@users.noreply.github.com>
2022-03-10 11:34:48 +0100

    ✨ Help/setup commands

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'help',
-
+✨ Help/setup commands
     execute(client, message) {
         message.channel.send(`To configure the ticket message **setup** and to get the bot latency **ping** ✅`);
     },
