31c328b3e902d53292ef27d40ce68f065edb3f5c
NOobSsam <68308311+NOobSsam@users.noreply.github.com>
2022-03-10 11:30:45 +0100

    ⚡️ Custom ID

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,1 @@
-module.exports = (client) => console.log(`${client.user.username} ready to manage tickets !`);w
+module.exports = (client) => console.log(`${client.user.username} ready to manage tickets !`);
